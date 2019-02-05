41da1a85fef961154846674e481d568fbd3caa99
Erastus Ruiru <erastusruiru@Erastuss-MacBook-Pro.local>
2019-02-05 17:17:41 +0300

    Updates

---
+++ precache-manifest.ec85b6e90b4ac24a741f6fd82e557f46.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bf2515a31126aef78a60",
+    "url": "/react/static/js/runtime~main.bf2515a3.js"
+  },
+  {
+    "revision": "89045ee4f828322c837e",
+    "url": "/react/static/js/main.89045ee4.chunk.js"
+  },
+  {
+    "revision": "bb1088f502250910469e",
+    "url": "/react/static/js/1.bb1088f5.chunk.js"
+  },
+  {
+    "revision": "89045ee4f828322c837e",
+    "url": "/react/static/css/main.31f150b6.chunk.css"
+  },
+  {
+    "revision": "5d5e59d60c7bef283c018552788a498c",
+    "url": "/react/index.html"
+  }
+];
